ce56800c71c22b8074db9f36549ce9c875da6af2
Pedro Bittencourt <p.f.bit@hotmail.com>
2016-09-12 22:39:29 -0300

    ta ai

--- LAB/TP1/LP/src/Pacote/ShowCommand.java
+++ LAB/TP1/LP/src/Pacote/ShowCommand.java
@@ -9,6 +9,32 @@ package Pacote;
  *
  * @author root
  */
-public class ShowCommand {
+public class ShowCommand extends Command{
+    
+    private Value<?> value;
+   
+    public ShowCommand(Value<?> value, int line){
+        super(line);
+        this.value = value;
+    }
+    
+    public void execute(){
+        if(value instanceof IntValue){
+            IntValue iv = (IntValue) value;
+            int n = iv.value;
+            System.out.println(n);
+        } else if (value instanceof StringValue){
+            StringValue sv = (StringValue) value;
+            String s = sv.value;
+            System.out.println(s);
+        } else if(value instanceof MatrixValue){
+            MatrixValue mv = (MatrixValue) value;
+            Matrix m = mv.value;
+            m.show();
+        } else {
+           //ERRO
+        }
+        
+    }
     
 }
